8d662b7137716dd5d7207b25bebdfab1020e1e1c
OTDE <mail@sethchapman.com>
2019-07-29 12:06:40 -0700

    Into the afternoon! 7/29
    CHANGES:
    -Outlined settings data
    
    TODO TODAY/TOMORROW:
    -Outline settings BLOC/frontend

--- lib/models/settings_data.dart
+++ lib/models/settings_data.dart
@@ -1,9 +1,53 @@
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 
 class Settings {
 
-  double radius;
-  DateTime startsBeforeDate;
+  final String _radius = 'distanceFromCenter';
+  final String _earlyDate = 'earliestTournamentDate';
+  final String _lateDate = 'latestTournamentDate';
 
+  /// Default values for the settings
+  final double _defaultRadius = 50.0;
+  final int _defaultEarlyDate = _daysFromNow(0);
+  final int _defaultLateDate = _daysFromNow(60);
+
+  static int _daysFromNow(int numOfDays) => DateTime.now().add(Duration(days: numOfDays)).millisecondsSinceEpoch;
+
+  Future<double> getRadiusInMiles() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    return preferences.getDouble(_radius) ?? _defaultRadius;
+  }
+
+  Future<bool> setRadiusInMiles(double radius) async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    return preferences.setDouble(_radius, radius);
+  }
+
+  Future<int> getEarlyDate() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    return preferences.getInt(_earlyDate) ?? _defaultEarlyDate;
+  }
+
+  Future<bool> setEarlyDate(int date) async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    return preferences.setInt(_earlyDate, date);
+  }
+
+  Future<int> getLateDate() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    return preferences.getInt(_lateDate) ?? _defaultLateDate;
+  }
+
+  Future<bool> setLateDate(int date) async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    return preferences.setInt(_lateDate, date);
+  }
 
 }
