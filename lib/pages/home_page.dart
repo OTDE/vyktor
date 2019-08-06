141ed4d78ad9325720daff6396f4ab0c6cf57d15
OTDE <mail@sethchapman.com>
2019-08-05 17:56:51 -0700

    Rebuilding launch stuff for all this deployment

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,14 +18,14 @@ class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
 /// The state of the [HomePage].
 ///
 /// On [initState], checks the phone's permissions,
 /// and computes if the app [_hasLocationPermissions].
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
 
   /// An indicator of if the app is allowed to track the phone's location.
   bool _hasLocationPermissions = false;
