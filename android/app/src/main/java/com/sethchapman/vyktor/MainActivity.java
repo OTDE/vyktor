dd6f89cfb935fc9e46b64254e9ba7bcc17078f3b
OTDE <mail@sethchapman.com>
2019-08-13 12:03:13 -0700

    iOS and android files.

---
+++ android/app/src/main/java/com/sethchapman/vyktor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sethchapman.vyktor;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
