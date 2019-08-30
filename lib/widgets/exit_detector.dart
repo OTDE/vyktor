6a69e76ae76713ac9e47bfd70863b8dd3f99f5c2
OTDE <mail@sethchapman.com>
2019-08-29 20:30:01 -0700

    Gestures fixed!! (forgot to add this)

---
+++ lib/widgets/exit_detector.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vyktor/blocs/blocs.dart';
+
+class ExitDetector extends StatefulWidget {
+  @override
+  _ExitDetectorState createState() => _ExitDetectorState();
+}
+
+class _ExitDetectorState extends State<ExitDetector> {
+  @override
+  Widget build(BuildContext context) {
+    final mapBloc = BlocProvider.of<MapDataBloc>(context);
+    final animBloc = BlocProvider.of<AnimatorBloc>(context);
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        print('tapped exit detector!');
+        mapBloc.dispatch(UnlockMap());
+        animBloc.dispatch(DeselectAll());
+      },
+      child: Container(
+        alignment: Alignment.center,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+      ),
+    );
+  }
+}
